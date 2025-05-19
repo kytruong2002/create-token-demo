83e92245de12abf8ce1d2766ee0b81b8782b8da3
kytruong2002 <ky.truong@apps-cyclone.com>
2025-05-19 14:20:51 +0700

    update code

--- src/templates/form.tsx
+++ src/templates/form.tsx
@@ -1,7 +1,5 @@
-import { useConnectWallet } from '@/hooks/useConnectWallet'
 import Header from '@/layouts/header'
 import { Container } from '@/utils/styles'
-import { Spin } from 'antd'
 import React from 'react'
 
 interface FormProps {
@@ -10,12 +8,10 @@ interface FormProps {
 }
 
 const Form = ({ children, title }: FormProps) => {
-  const { isLoading } = useConnectWallet()
   return (
     <>
       <Header title={title} />
       <Container>{children}</Container>
-      <Spin spinning={isLoading} fullscreen />
     </>
   )
 }
--- src/types/token.ts
+++ src/types/token.ts
@@ -1,24 +1,3 @@
-export interface CreateTokenRequest {
-  txHash: string
-  description: string
-  websiteUrl?: string
-  telegramUrl?: string
-  discordUrl?: string
-  xUrl?: string
-}
-
-export interface TokenRequest {
-  name: string
-  symbol: string
-  decimals: number
-  supply: number
-  description: string
-  websiteUrl?: string
-  telegramUrl?: string
-  discordUrl?: string
-  xUrl?: string
-}
-
 export interface Token {
   _id?: string
   name: string
