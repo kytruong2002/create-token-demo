ad456748322caeb80eb8947db0a2cceedf536a46
kytruong2002 <ky.truong@apps-cyclone.com>
2025-05-23 08:25:42 +0700

    fix format token

--- src/components/formatToken/index.tsx
+++ src/components/formatToken/index.tsx
@@ -9,11 +9,26 @@ const FormatToken = ({ token, symbol }: FormatTokenProps) => {
   const valueStr = typeof token === 'number' ? token.toFixed(18) : token.toString()
   const [intPart, decimalPartRaw = ''] = valueStr.split('.')
 
+  const first8Digits = decimalPartRaw.slice(0, 8)
+  const remainingDigits = decimalPartRaw.slice(8)
+
+  const isTrailingZeroAfter8 = remainingDigits.split('').every((char) => char === '0')
+
   if (!decimalPartRaw || /^0+$/.test(decimalPartRaw)) {
     return (
       <Tooltip title={`$${valueStr}`}>
         <span>
           ${intPart}.0 {symbol}
+        </span>
+      </Tooltip>
+    )
+  }
+
+  if (isTrailingZeroAfter8) {
+    return (
+      <Tooltip title={`$${valueStr}`}>
+        <span>
+          ${intPart}.{first8Digits} {symbol}
         </span>
       </Tooltip>
     )
