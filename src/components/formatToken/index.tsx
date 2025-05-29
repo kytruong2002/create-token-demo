504c4fa43cb17ebaf4afaf5632b10639f36309e6
kytruong2002 <ky.truong@apps-cyclone.com>
2025-05-29 09:42:50 +0700

    refactor: fix code format token

--- src/components/formatToken/index.tsx
+++ src/components/formatToken/index.tsx
@@ -8,35 +8,43 @@ type FormatTokenProps = {
 const FormatToken = ({ token, symbol }: FormatTokenProps) => {
   const valueStr = token.toString()
   const [intPart, decimalPartRaw = ''] = valueStr.split('.')
-  const trimmedDecimal = decimalPartRaw.replace(/0+$/, '')
+  const trimmedDecimal = decimalPartRaw.replace(/0+$/, '').trim()
 
   if (trimmedDecimal.length <= 7) {
+    if (trimmedDecimal.length <= 0) {
+      return (
+        <span>
+          {`${intPart}.0`} {symbol}
+        </span>
+      )
+    }
+
     return (
       <span>
         {valueStr} {symbol}
       </span>
     )
-  } else {
-    const firstNonZeroIndex = [...trimmedDecimal].findIndex((char) => char !== '0')
-    if (firstNonZeroIndex > 3) {
-      return (
-        <Tooltip title={`${valueStr}`}>
-          <span>
-            {intPart}.0<sub style={{ fontSize: '0.8em' }}>{firstNonZeroIndex}</sub>
-            {trimmedDecimal.slice(firstNonZeroIndex, firstNonZeroIndex + 4)} {symbol}
-          </span>
-        </Tooltip>
-      )
-    } else {
-      return (
-        <Tooltip title={`${valueStr}`}>
-          <span>
-            {Number(token).toFixed(7).toString()} {symbol}
-          </span>
-        </Tooltip>
-      )
-    }
   }
+
+  const firstNonZeroIndex = [...trimmedDecimal].findIndex((char) => char !== '0')
+  if (firstNonZeroIndex > 3) {
+    return (
+      <Tooltip title={`${valueStr}`}>
+        <span>
+          {intPart}.0<sub style={{ fontSize: '0.8em' }}>{firstNonZeroIndex}</sub>
+          {trimmedDecimal.slice(firstNonZeroIndex, firstNonZeroIndex + 4)} {symbol}
+        </span>
+      </Tooltip>
+    )
+  }
+
+  return (
+    <Tooltip title={`${valueStr}`}>
+      <span>
+        {Number(token).toFixed(7).toString()} {symbol}
+      </span>
+    </Tooltip>
+  )
 }
 
 export default FormatToken
