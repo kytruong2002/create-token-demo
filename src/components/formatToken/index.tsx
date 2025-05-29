d5329dffa893ba83a4cd67b49ef360433624a40a
kytruong2002 <ky.truong@apps-cyclone.com>
2025-05-29 09:35:02 +0700

    refactor: update format Token

--- src/components/formatToken/index.tsx
+++ src/components/formatToken/index.tsx
@@ -6,46 +6,37 @@ type FormatTokenProps = {
 }
 
 const FormatToken = ({ token, symbol }: FormatTokenProps) => {
-  const valueStr = typeof token === 'number' ? token.toFixed(18) : token.toString()
+  const valueStr = token.toString()
   const [intPart, decimalPartRaw = ''] = valueStr.split('.')
+  const trimmedDecimal = decimalPartRaw.replace(/0+$/, '')
 
-  const first8Digits = decimalPartRaw.slice(0, 8)
-  const remainingDigits = decimalPartRaw.slice(8)
-
-  const isTrailingZeroAfter8 = remainingDigits.split('').every((char) => char === '0')
-
-  if (!decimalPartRaw || /^0+$/.test(decimalPartRaw)) {
+  if (trimmedDecimal.length <= 7) {
     return (
-      <Tooltip title={`${valueStr}`}>
-        <span>
-          {intPart}.0 {symbol}
-        </span>
-      </Tooltip>
-    )
-  }
-
-  if (isTrailingZeroAfter8) {
-    return (
-      <Tooltip title={`${valueStr}`}>
-        <span>
-          {intPart}.{first8Digits} {symbol}
-        </span>
-      </Tooltip>
-    )
-  }
-
-  const firstNonZeroIndex = [...decimalPartRaw].findIndex((char) => char !== '0')
-  const subDigit = decimalPartRaw[firstNonZeroIndex]
-  const rest = decimalPartRaw.slice(firstNonZeroIndex + 1, firstNonZeroIndex + 5)
-
-  return (
-    <Tooltip title={`${valueStr}`}>
       <span>
-        {intPart}.0<sub style={{ fontSize: '0.8em' }}>{subDigit}</sub>
-        {rest} {symbol}
+        {valueStr} {symbol}
       </span>
-    </Tooltip>
-  )
+    )
+  } else {
+    const firstNonZeroIndex = [...trimmedDecimal].findIndex((char) => char !== '0')
+    if (firstNonZeroIndex > 3) {
+      return (
+        <Tooltip title={`${valueStr}`}>
+          <span>
+            {intPart}.0<sub style={{ fontSize: '0.8em' }}>{firstNonZeroIndex}</sub>
+            {trimmedDecimal.slice(firstNonZeroIndex, firstNonZeroIndex + 4)} {symbol}
+          </span>
+        </Tooltip>
+      )
+    } else {
+      return (
+        <Tooltip title={`${valueStr}`}>
+          <span>
+            {Number(token).toFixed(7).toString()} {symbol}
+          </span>
+        </Tooltip>
+      )
+    }
+  }
 }
 
 export default FormatToken
